d02513f65abb12f1349659501906e46d54057896
Robb Hedrick <robbhedrick@gmail.com>
2013-02-20 14:03:18 -0500

    changed DOM for afix side box for tablet and up.

--- js/app.js
+++ js/app.js
@@ -57,11 +57,11 @@ function calcRoute(location) {
 function codeAddress(address) {
 	geocoder.geocode( { 'address': address}, function(results, status) {
 		if (status == google.maps.GeocoderStatus.OK) {
-			//map.setCenter(results[0].geometry.location);
+			/*map.setCenter(results[0].geometry.location);
 			var marker = new google.maps.Marker({
         		map: map,
         		position: results[0].geometry.location
-        	});
+        	});*/
         	calcRoute(results[0].geometry.location);
         } else {
     		alert("Geocode was not successful for the following reason: " + status);
